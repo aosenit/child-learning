f191fdfc0995c8302495687c033771e5466ce7de
oseni ade <oseniadedoyin22@gmail.com>
2023-01-03 11:56:30 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Todo from "./Todo";
+
+function App() {
+  return (
+    <div className="">
+      <h1 className="text-red-500">hello world</h1>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.tsx
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from "uuid";
+import React from "react";
+
+const Todo = () => {
+  return <div>Todo</div>;
+};
+
+export default Todo;
